5483576b6f599f0f1daa7e2b682ba56346440156
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-04-05 23:43:53 +0900

    02 custom font

---
+++ 02-CustomFont/02-CustomFont/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  02-CustomFont
+//
+//  Created by 松尾和昭 on 05/04/2016.
+//  Copyright © 2016 kazucocoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 02-CustomFont/02-CustomFontTests/_2_CustomFontTests.swift
@@ -0,0 +1,36 @@
+//
+//  _2_CustomFontTests.swift
+//  02-CustomFontTests
+//
+//  Created by 松尾和昭 on 05/04/2016.
+//  Copyright © 2016 kazucocoa. All rights reserved.
+//
+
+import XCTest
+@testable import _2_CustomFont
+
+class _2_CustomFontTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
