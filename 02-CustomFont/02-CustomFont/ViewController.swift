4b17e3d297b0165485661292dcca302f7336cecd
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-04-06 08:07:10 +0900

    arrange a bit

--- 02-CustomFont/02-CustomFont/ViewController.swift
+++ 02-CustomFont/02-CustomFont/ViewController.swift
@@ -1,18 +1,38 @@
-//
-//  ViewController.swift
-//  02-CustomFont
-//
-//  Created by 松尾和昭 on 05/04/2016.
-//  Copyright © 2016 kazucocoa. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let data = ["30 days swif", "やっほー"]
+
+    let fontNames = ["MFTongXin_Noncommercial-Regular", "MFJinHei_Noncommercial-Regular", "MFZhiHei_Noncommercial-Regular"]
+
+    var fontRowIndex = 0
+
+    @IBOutlet weak var changeFontButton: UIButton!
+    @IBOutlet weak var fontTableView: UITableView!
+
+    @IBAction func changeFontDidTouch(sender: AnyObject) {
+        fontRowIndex = (fontRowIndex + 1) % 3
+        fontTableView.reloadData()
+    }
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        fontTableView.dataSource = self
+        fontTableView.delegate = self
+
+        UIFont.familyNames().forEach { family in
+            UIFont.fontNamesForFamilyName(family).forEach { font in
+                //print(font)
+            }
+        }
+
+        changeFontButton.layer.cornerRadius = 55
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = fontTableView.dequeueReusableCellWithIdentifier("FontCell", forIndexPath: indexPath)
+
+        let text = data[indexPath.row]
+
+        cell.textLabel?.text = text
+        cell.textLabel?.textColor = UIColor.blackColor()
+        cell.textLabel?.font = UIFont(name: self.fontNames[fontRowIndex], size: 16)
+
+        return cell
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
 
 }
 
